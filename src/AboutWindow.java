607367dcff2a4b0e5bcbc5982ced484376cd3b26
Dmitry Gurylev <gurylev.dmitrii@mail.ru>
2021-01-14 00:08:41 +0300

    url inst added

--- src/AboutWindow.java
+++ src/AboutWindow.java
@@ -12,33 +12,40 @@ public class AboutWindow extends JFrame {
         aboutWindowFrame.setSize(600,400);
         aboutWindowFrame.setLocationRelativeTo(null);
         JPanel aboutPanel = new JPanel();
-        aboutPanel.setLayout(new BoxLayout(aboutPanel, BoxLayout.Y_AXIS));
         aboutWindowFrame.add(aboutPanel);
-
-
+        aboutPanel.setLayout(new BoxLayout(aboutPanel, BoxLayout.Y_AXIS));
+        JLabel headImage;
 
         try {
             String imagePath = "C:\\Users\\NewUser\\Desktop\\calculator\\src\\resources\\info.png";
             BufferedImage img = ImageIO.read(new File(imagePath));
             Image scaledImg = img.getScaledInstance(70,70,Image.SCALE_SMOOTH);
-            JLabel headImage = new JLabel(new ImageIcon(scaledImg));
+            headImage = new JLabel(new ImageIcon(scaledImg));
             headImage.setSize(70,70);
+            headImage.setBorder(BorderFactory.createEmptyBorder(10,10,0,10));
             aboutPanel.add(headImage);
         }
         catch (IOException e) {
             e.printStackTrace();
         }
+
         JLabel description = new JLabel();
-        aboutPanel.add(description);
-        description.setText("<html><h2>What is this?</h2>" +
+        description.setText("<html><h2 align=center>What is this?</h2>" +
                 "This calculator has been created by Dmitry Gurylev (Moscow) " +
                 "as a pet project. <br><br>" +
                 "e-mail: \"gurylev.dmitrii@mail.ru\"  <br>" +
-                "inst: \"@dmitrygurylev\" <br>" +
+                "inst: \"" +
+                "<a href=\"https://www.instagram.com/dmitrygurylev/\">@dmitrygurylev</a>" +
+                "\" <br>" +
                 "facebook: \"gurylev.dmitrii\"  <br>"+
                 " </html>");
+        description.setBorder(BorderFactory.createEmptyBorder(0,10,10,10));
+        aboutPanel.add(description);
+
         aboutWindowFrame.setVisible(true);
         aboutWindowFrame.setResizable(false);
+        aboutPanel.setEnabled(true);
+
     }
 }
 
